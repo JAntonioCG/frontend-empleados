35202442307a3cc0107c0b0c8d38a1d09191c7ee
Antonio Cas <juanantoniocastanongarcia@gmail.com>
2024-08-23 08:41:15 -0600

    plantilla

---
+++ components/empleados/loginCard.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-card elevation="0" color="blue darken-8">
+    <v-card-title>
+      Bienvenido
+    </v-card-title>
+    <v-card-text>Formulario</v-card-text>
+    <v-card-actions>Botones</v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+}
+</script>
---
+++ layouts/login.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-row align="center" justify="center" class="login">
+        <nuxt />
+      </v-row>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+.login{
+  width: 100%;
+  height: 100vh;
+  background-color: rgb(47, 58, 68);
+}
+</style>
